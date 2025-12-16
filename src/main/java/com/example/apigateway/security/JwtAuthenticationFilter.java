0b8c9bd08aa22762093a0a09924d73327c87c571
m1crovolnovka <denisdevyaltovski@gmail.com>
2025-12-16 16:53:22 +0300

    add security filter

---
+++ src/main/java/com/example/apigateway/security/JwtAuthenticationFilter.java
@@ -0,0 +1,72 @@
+package com.example.apigateway.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    private final SecretKey key;
+
+    public JwtAuthenticationFilter(@Value("${jwt.secret.key}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private static final List<String> OPEN_API_ENDPOINTS = List.of(
+            "/api/auth/login",
+            "/api/auth/register"
+    );
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getPath().toString();
+        if (OPEN_API_ENDPOINTS.stream().anyMatch(path::contains)) {
+            return chain.filter(exchange);
+        }
+        if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+            return this.onError(exchange, "Missing Authorization header", HttpStatus.UNAUTHORIZED);
+        }
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return this.onError(exchange, "Invalid Authorization scheme", HttpStatus.UNAUTHORIZED);
+        }
+        String token = authHeader.substring(7);
+        try {
+          Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return chain.filter(exchange);
+
+        } catch (JwtException e) {
+            return this.onError(exchange, "Invalid or expired JWT token: " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            return this.onError(exchange, "Internal token processing error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+}
